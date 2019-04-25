522661035669272f726d7502d2fff67b747c1eea
Charles Rowland <charlesrowland@Charless-MacBook-Pro.local>
2019-04-24 20:24:09 -0600

    added the mtg sdk because I forgot to do that before. oops.

--- app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/MainActivity.java
+++ app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void todoCrap() {
+        /*
+        TODO: Set up timber as a function so it outputs 'fart' as a tag (in utils file) *maybe*        TODO: set up retrofit class file
+        TODO: set up retrofit interface file
+        TODO: set up Room with some dummy data (LiveData, Doa, Repository, Database, etc)
+        */
     }
 }
---
+++ app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/utils/AllMyConstants.java
@@ -0,0 +1,6 @@
+package com.ragingclaw.mtgcubedraftsimulator.utils;
+
+public class AllMyConstants {
+    // funny story. When I was a kid my aunt watched a Soap called All My Children.
+    // so here we are... All My Constants.
+}
