1c25fcecfb27b71bb2e9db87c7042a3c800ac264
gundamboy <burlpocket631>
2019-05-01 17:01:43 -0600

    added a login activity and had to update the google-serivces.json file with a new ssh1 key

---
+++ app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/activities/LoginActivty.java
@@ -0,0 +1,16 @@
+package com.ragingclaw.mtgcubedraftsimulator.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ragingclaw.mtgcubedraftsimulator.R;
+
+public class LoginActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activty);
+    }
+}
