2647e207d69a43a7a11ca0467dd24fd48597ad42
Charles Rowland <rowland.charles@gmail.com>
2019-06-05 22:21:08 -0600

    signing shit

--- app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/activities/LoginActivity.java
+++ app/src/main/java/com/ragingclaw/mtgcubedraftsimulator/activities/LoginActivity.java
@@ -1,64 +1,42 @@
 package com.ragingclaw.mtgcubedraftsimulator.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.ragingclaw.mtgcubedraftsimulator.BuildConfig;
 import com.ragingclaw.mtgcubedraftsimulator.R;
-import com.ragingclaw.mtgcubedraftsimulator.database.MagicCard;
 import com.ragingclaw.mtgcubedraftsimulator.fragments.CreateAccountFragment;
 import com.ragingclaw.mtgcubedraftsimulator.fragments.EmailPasswordFragment;
 import com.ragingclaw.mtgcubedraftsimulator.fragments.LoginFragment;
-import com.ragingclaw.mtgcubedraftsimulator.models.MagicCardViewModel;
-import com.ragingclaw.mtgcubedraftsimulator.utils.NotLoggingTree;
-
 
 import java.util.Arrays;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
-
-import static com.ragingclaw.mtgcubedraftsimulator.utils.AllMyConstants.RC_SIGN_IN;
 
 public class LoginActivity extends AppCompatActivity implements
         LoginFragment.OnFragmentInteractionListener,
         EmailPasswordFragment.OnFragmentInteractionListener,
         CreateAccountFragment.OnFragmentInteractionListener {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    ActionBar actionBar;
     public static final String ANONYMOUS = "anonymous";
     public static final int RC_SIGN_IN = 1;
     private String mUsername;
@@ -72,12 +50,6 @@ public class LoginActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-
-        toolbar.setTitle(getString(R.string.main_activity_title));
-        setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-        assert actionBar != null;
-        actionBar.setTitle(Html.fromHtml("<font color=\"#555555\">" + getString(R.string.app_name) + "</font>"));
 
         // Initialize FireBase Auth
         mAuth = FirebaseAuth.getInstance();
